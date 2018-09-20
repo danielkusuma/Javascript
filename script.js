3a22e92cf4673461517fe6bcc89e622f290ee61a
Daniel Kusuma <d@nielengineer.com>
2018-09-20 20:55:12 +1000

    commit

--- script.js
+++ script.js
@@ -190,7 +190,8 @@ console.log(john.indexOf(23));
 var isDesigner = john.indexOf('designer') === -1 ? 'John is not a designer' : 'John is a designer';
 console.log(isDesigner);
 */
-/*var bill = [124, 48, 268];
+/*
+var bill = [124, 48, 268];
 var totalPaid = new Array();
 function calculateTotalPaid (bill) {
     if (bill < 50) {
@@ -208,3 +209,4 @@ calculateTotalPaid(bill[1])
 calculateTotalPaid(bill[2])
 console.log(totalPaid);
 */
+// Object & Properties
